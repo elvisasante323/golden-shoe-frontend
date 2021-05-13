0da804add7239754e7be02e90b67ebd45ce94c7f
elvisasante5@yahoo.com <elvisasante5@yahoo.com>
2021-05-13 12:40:24 +0100

    Finished with creating the carousel

---
+++ golden-shoe/src/components/Carousel.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-carousel
+  id="carousel-1"
+  :interval="3000"
+  indicators
+  fade
+  controls
+  img-width="1024"
+  img-height="480"
+  no-wrap
+  >
+    <b-carousel-slide :img-src="require('../assets/trainer-1.png')">
+      <b-jumbotron header="With His Sixth Ring, Legend Status is Attained" lead="Only real legends can wear these kicks">
+        <b-button pill variant="outline-light" href="#" size="lg">More Info</b-button>
+      </b-jumbotron>
+    </b-carousel-slide>
+
+    <b-carousel-slide :img-src="require('../assets/trainer-2.png')">
+      <b-jumbotron header="With His Sixth Ring, Legend Status is Attained" lead="Only real legends can wear these kicks">
+        <b-button pill variant="outline-light" href="#" size="lg">More Info</b-button>
+      </b-jumbotron>
+    </b-carousel-slide>
+  </b-carousel>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+  mounted () {
+    const srOnly = document.querySelectorAll('.sr-only');
+    srOnly[0].innerHTML = '';
+    srOnly[1].innerHTML = '';
+  }
+}
+</script>
+
+<style scoped>
+.jumbotron {
+  position: relative;
+  bottom: 25em;
+}
+
+</style>
--- golden-shoe/src/views/Home.vue
+++ golden-shoe/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="home">
     <navigationBar />
+    <carousel />
   </div>
 </template>
 
 <script>
 import NavigationBar from "../components/NavigationBar";
+import Carousel from "../components/Carousel";
 export default {
   name: "Home",
   components: {
-    NavigationBar
+    NavigationBar,
+    Carousel
   },
 };
 </script>
